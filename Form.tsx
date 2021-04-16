2f535d14096023576608b23405326ffabae3b441
SurezVijay <v.suresh2907@gmail.com>
2021-04-16 13:51:52 +0800

    Initial commit

---
+++ Form.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import IUser from './IUser';
+
+interface IProps {};
+
+interface IState {
+  users : IUser 
+};
+
+class Form extends React.Component<IProps,IState> {
+  constructor(props:IProps){
+    super(props);
+    this.state = {
+      users : {} as IUser
+    }
+  }
+
+inputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+   this.setState({
+     users : {
+       ...this.state.users,
+       [event.target.name] : event.target.value
+     }
+   })
+}
+
+  render(){
+    return(
+      <div>
+      <pre>{JSON.stringify(this.state.users)} </pre>
+       <section className="mt-5">
+          <div className="container">
+             <div className="row">
+             <div className="col-md-6">
+               <div className="card">
+                <div className="card-header bg-primary text-white">
+                  <h3 className="card-title"> React Form Binding </h3>
+                </div>   
+                <div className="card-body">
+                  <form>
+                   <div className="form-control-group">
+                   <label className="form-control-item mr-4 mb-3"> UserName </label>
+                   <input type="text" name="username" 
+                   value={this.state.users.username}
+                   onChange={this.inputChange}
+                   className="form-control-item"/>
+                   </div>
+                    <div className="form-control-group">
+                   <label className="form-control-item mr-2"> Email Address </label>
+                   <input type="text" name="email" 
+                   value={this.state.users.email}
+                   onChange={this.inputChange}
+                   className="form-control-item"/>
+                   </div>
+                  </form>
+                </div>              
+               </div>
+               </div>
+             </div>
+          </div>
+       </section>
+      </div>
+    );
+  }
+}
+
+export default Form ;
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-form-binding
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-form-binding)
